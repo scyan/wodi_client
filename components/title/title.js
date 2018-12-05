7135295e463e682c8c0c17618653b5640bd0d260
scyan <ydd-jd@163.com>
2018-12-05 20:55:49 +0800

    update: style

---
+++ components/title/title.js
@@ -0,0 +1,30 @@
+// components/title/title.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    status: {
+      type: Number,
+      value: 1
+    },
+    userName:{
+      type: String,
+      value:''
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
