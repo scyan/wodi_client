0862c463e7e0fe95d9adeff7447b65f447060f62
scyan <ydd-jd@163.com>
2018-12-06 20:48:38 +0800

    update: actions

---
+++ components/actions/index.js
@@ -0,0 +1,30 @@
+// components/actions/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    status:{
+      type: Number,
+      value:1
+    },
+    user:{
+      type: Object,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
